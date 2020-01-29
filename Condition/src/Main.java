099cd5bfcf7500f9b05a1135478ff3dc4fc38a09
Mingthean Lay <thean@Mingtheans-MacBook-Pro.local>
2020-01-29 19:32:18 +0700

    first commit

---
+++ Condition/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int objective = 5;
+        int userInput;
+        boolean condition = true;
+        Scanner scan = new Scanner(System.in);
+        while(condition) {
+            System.out.println("Guess the number: ");
+            userInput = scan.nextInt();
+            if(userInput < objective) {
+                System.out.println("HIGHER");
+            }
+            else if (userInput > objective) {
+                System.out.println("LOWER");
+            }
+            else if (objective == userInput){
+                System.out.println("CONGRATS");
+                condition = false;
+            }
+        }
+    }
+}
