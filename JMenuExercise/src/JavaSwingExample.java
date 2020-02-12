dd066a4bb8ea5f780e075c8641ad676ebb91ea8c
Thean <mingthean.lay@gmail.com>
2020-02-12 13:07:24 +0700

    set function for JMenu Exercise

--- JMenuExercise/src/JavaSwingExample.java
+++ JMenuExercise/src/JavaSwingExample.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.EventListener;
 
-public class JavaSwingExample extends JFrame {
+public class JavaSwingExample extends JFrame implements ActionListener {
 
     JMenu fileMenu, editMenu, aboutMenu, linkMenu;
     JMenuBar menuBar = new JMenuBar();
@@ -8,6 +16,7 @@ public class JavaSwingExample extends JFrame {
             copyMenuItem, cutMenuItem, pasteMenuItem;
     JCheckBox showAboutChk = new JCheckBox("Show About",true);
     JRadioButton showLinksRad = new JRadioButton("Show Links",true);
+    JLabel showSelected = new JLabel();
 
     public JavaSwingExample() {
 
@@ -34,21 +43,52 @@ public class JavaSwingExample extends JFrame {
         fileMenu.addSeparator();
         fileMenu.add(showLinksRad);
         fileMenu.addSeparator();
-        
+
         editMenu.add(cutMenuItem);
         editMenu.add(copyMenuItem);
         editMenu.add(pasteMenuItem);
 
         menuBar.add(fileMenu);
         menuBar.add(editMenu);
-        menuBar.add(aboutMenu);
         menuBar.add(linkMenu);
+        menuBar.add(aboutMenu);
         setJMenuBar(menuBar);
 
         new JFrame("Java Swing Examples");
-        setSize(500,500);
+
+        showSelected.setBounds(100,100,10,10);
+        add(showSelected);
+
+        setSize(500, 500);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        showAboutChk.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                aboutMenu.setVisible(showAboutChk.isSelected());
+            }
+        });
+
+        showLinksRad.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                linkMenu.setVisible(showLinksRad.isSelected());
+            }
+        });
+
+        cutMenuItem.addActionListener(this::actionPerformed);
+        pasteMenuItem.addActionListener(this::actionPerformed);
+        copyMenuItem.addActionListener(this::actionPerformed);
+        newMenuItem.addActionListener(this::actionPerformed);
+        openMenuItem.addActionListener(this::actionPerformed);
+        saveMenuItem.addActionListener(this::actionPerformed);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        showSelected.setText(e.getActionCommand() + " is selected");
     }
 
     public static void main(String[] args) {
